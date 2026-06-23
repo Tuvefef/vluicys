456fdd1662e0e4adbc62a635f5f0a3e2e5fa74d5
unknown <gabo129013@gmail.com>
2026-06-23 15:43:14 -0600

    Initial commit

---
+++ src/main/java/net/tuvefe/vluicys/VluicysInitClient.java
@@ -0,0 +1,11 @@
+package net.tuvefe.vluicys;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VluicysInitClient implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient()
+    {
+    }
+}
---
+++ src/main/java/net/tuvefe/vluicys/item/ItemClass.java
@@ -0,0 +1,27 @@
+package net.tuvefe.vluicys.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.tuvefe.vluicys.Vluicys;
+
+public class ItemClass
+{
+    public static final Item VLUICYS_MUSHROOM = registerItem("vluicys_mushroom", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item)
+    {
+        return Registry.register(Registries.ITEM, Identifier.of(Vluicys.MOD_ID, name), item);
+    }
+
+    public static void itemRegister()
+    {
+        Vluicys.LOGGER.info("ya esta puto xdxdxd" + Vluicys.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(VLUICYS_MUSHROOM);
+        });
+    }
+}
